d6496fbad5c6ce9450b776175b1733e00bd43bfc
Benjamin Kvinen Røiland <benjamin.roiland@hotmail.no>
2026-09-21 08:38:52 +0200

    global.css is not working as intended somehow

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,32 @@
-import "@/global.css";
-import { Link } from "expo-router";
+import { HOME_BALANCE, HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-7xl font-sans-extrabold">Home</Text>
-      <Link
-        href="/onboarding"
-        className="mt-4 rounded font-sans-bold bg-accent text-white p-4"
-      >
-        Go to onboarding
-      </Link>
-      <Link
-        href="/(auth)/sign-in"
-        className="mt-4 rounded font-sans-bold bg-accent text-white p-4"
-      >
-        Go to Sign in
-      </Link>
-      <Link
-        href="/(auth)/sign-up"
-        className="mt-4 rounded font-sans-bold bg-accent text-white p-4"
-      >
-        Go to Sign up
-      </Link>
+    <SafeAreaView className="flex-1 bg-background">
+      <View className="flex-1 p-5">
+        <View className="home-header">
+          <View className="home-user">
+            <Image source={images.avatar} className="home-avatar" />
+            <Text className="home-user-name">{HOME_USER.name}</Text>
+          </View>
+          <Image source={icons.add} className="home-add-icon" />
+        </View>
+
+        <View className="home-balance-card">
+          <Text className="home-balance-label">Balance</Text>
+
+          <View className="home-balance-row">
+            <Text className="home-balance-amount">{HOME_BALANCE.amount}</Text>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
