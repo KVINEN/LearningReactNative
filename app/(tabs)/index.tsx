2a04d5ac73003abb21a664f754418ced711f8369
Benjamin Kvinen Røiland <benjamin.roiland@hotmail.no>
2026-09-22 06:54:22 +0200

    started on components and fixed global.css problem

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,10 @@
+import ListHeading from "@/components/ListHeading";
+import UpcommingSubscriptionCard from "@/components/UpcommingSubscriptionCard";
 import { HOME_BALANCE, HOME_USER } from "@/constants/data";
 import { icons } from "@/constants/icons";
 import images from "@/constants/images";
+import { formatCurrency } from "@/libs/utils";
+import dayjs from "dayjs";
 import { styled } from "nativewind";
 import { Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
@@ -9,23 +13,32 @@ const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 bg-background">
-      <View className="flex-1 p-5">
-        <View className="home-header">
-          <View className="home-user">
-            <Image source={images.avatar} className="home-avatar" />
-            <Text className="home-user-name">{HOME_USER.name}</Text>
-          </View>
-          <Image source={icons.add} className="home-add-icon" />
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar" />
+          <Text className="home-user-name">{HOME_USER.name}</Text>
         </View>
+        <Image source={icons.add} className="home-add-icon" />
+      </View>
 
-        <View className="home-balance-card">
-          <Text className="home-balance-label">Balance</Text>
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Balance</Text>
 
-          <View className="home-balance-row">
-            <Text className="home-balance-amount">{HOME_BALANCE.amount}</Text>
-          </View>
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">
+            {formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className="home-balance-date">
+            {dayjs(HOME_BALANCE.nextRenewalDate).format("DD/MM")}
+          </Text>
         </View>
+      </View>
+      <View>
+        <ListHeading title="upcomming" />
+      </View>
+      <View>
+        <UpcommingSubscriptionCard title="All Subscriptions" />
       </View>
     </SafeAreaView>
   );
---
+++ components/UpcommingSubscriptionCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const UpcommingSubscriptionCard = () => {
+  return (
+    <View>
+      <Text>UpcommingSubscriptionCard</Text>
+    </View>
+  );
+};
+
+export default UpcommingSubscriptionCard;
